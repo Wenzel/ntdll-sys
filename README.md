137dd4341f96111970ee82e959e2d4b53df29c04
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-11-13 09:52:05 +0100

    testing build with clang include

---
+++ README.md
@@ -0,0 +1,37 @@
+# ntdll-sys
+
+[![standard-readme compliant](https://img.shields.io/badge/readme%20style-standard-brightgreen.svg?style=flat-square)](https://github.com/RichardLitt/standard-readme)
+
+> Rust unsafe bindings for ntdll
+
+## Table of Contents
+
+- [Requirements](#requirements)
+- [References](#references)
+- [Maintainers](#maintainers)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Requirements
+
+- `cargo`
+- `rustc`
+- `clang`
+
+## References
+
+- [ReactOS](https://github.com/reactos/reactos)
+
+## Maintainers
+
+[@Wenzel](https://github.com/Wenzel)
+
+## Contributing
+
+PRs accepted.
+
+Small note: If editing the Readme, please conform to the [standard-readme](https://github.com/RichardLitt/standard-readme) specification.
+
+## License
+
+[GNU General Public License v3.0](https://github.com/Wenzel/pyvmidbg/blob/master/LICENSE)
---
+++ build.rs
@@ -0,0 +1,34 @@
+extern crate bindgen;
+use std::path::Path;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+
+    // what library to link with
+    println!("cargo:rustc-link-lib=ntdll");
+
+    let build_source = Path::new(file!());
+    let project_root = build_source.parent();
+    let include_dir = project_root.join("src/include")
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("src/wrapper.h")
+        .clang_arg(format!("-I{}", include_dir))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
